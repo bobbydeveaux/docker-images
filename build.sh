9bc5564d1f83321b27a1379bf06e87a8e8435f9c
Bobby DeVeaux <bdeveaux@ukcloud.com>
2016-12-22 11:34:59 +0000

    adding version prefix

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+VERSION=1
 CONTAINER=$1
 BUILD_NUMBER=$2
 
+
+
 docker build ./$CONTAINER -t bobbydvo/ukc_$CONTAINER:latest
-docker tag bobbydvo/ukc_$CONTAINER:latest  bobbydvo/ukc_$CONTAINER:$BUILD_NUMBER
+docker tag bobbydvo/ukc_$CONTAINER:latest  bobbydvo/ukc_$CONTAINER:$VERSION.$BUILD_NUMBER
 
 docker push bobbydvo/ukc_$CONTAINER:latest
-docker push bobbydvo/ukc_$CONTAINER:$BUILD_NUMBER
+docker push bobbydvo/ukc_$CONTAINER:VERSION.$BUILD_NUMBER
