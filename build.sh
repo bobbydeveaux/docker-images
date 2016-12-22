9f6f39e65cc009bbe5537664bf411c95a5637065
Bobby DeVeaux <bdeveaux@ukcloud.com>
2016-12-22 11:35:52 +0000

    adding version correctly

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ docker build ./$CONTAINER -t bobbydvo/ukc_$CONTAINER:latest
 docker tag bobbydvo/ukc_$CONTAINER:latest  bobbydvo/ukc_$CONTAINER:$VERSION.$BUILD_NUMBER
 
 docker push bobbydvo/ukc_$CONTAINER:latest
-docker push bobbydvo/ukc_$CONTAINER:VERSION.$BUILD_NUMBER
+docker push bobbydvo/ukc_$CONTAINER:$VERSION.$BUILD_NUMBER
