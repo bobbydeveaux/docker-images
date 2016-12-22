239d219f1746d78a6df0d1ce8b2bd901b02bf9f5
Bobby DeVeaux <bdeveaux@ukcloud.com>
2016-12-22 10:37:39 +0000

    adding build number

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 CONTAINER=$1
 
 docker build ./$CONTAINER -t bobbydvo/ukc_$CONTAINER:latest
+docker tag bobbydvo/ukc_$CONTAINER:latest  bobbydvo/ukc_$CONTAINER:$BUILD_NUMBER
 
 docker push bobbydvo/ukc_$CONTAINER:latest
+docker push bobbydvo/ukc_$CONTAINER:$BUILD_NUMBER
