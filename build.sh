a77ca2e39e7bddb4f7f9e8a5a81d8be13c5dd539
Bobby DeVeaux <me@bobbyjason.co.uk>
2017-02-06 10:07:31 +0000

    adding build all option

--- build.sh
+++ build.sh
@@ -4,10 +4,17 @@ VERSION=1
 CONTAINER=$1
 BUILD_NUMBER=$2
 
-
+if [[ $CONTAINER == 'all' ]];
+	then
+		for CONTAINER in php-fpm nginx dynamodb;
+		do
+			docker build ./$CONTAINER -t bobbydvo/ukc_$CONTAINER:latest
+			docker tag bobbydvo/ukc_$CONTAINER:latest  bobbydvo/ukc_$CONTAINER:$VERSION.$BUILD_NUMBER
+		done
+		exit
+fi
 
 docker build ./$CONTAINER -t bobbydvo/ukc_$CONTAINER:latest
 docker tag bobbydvo/ukc_$CONTAINER:latest  bobbydvo/ukc_$CONTAINER:$VERSION.$BUILD_NUMBER
-
 docker push bobbydvo/ukc_$CONTAINER:latest
 docker push bobbydvo/ukc_$CONTAINER:$VERSION.$BUILD_NUMBER
